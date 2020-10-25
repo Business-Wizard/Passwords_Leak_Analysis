213dc9d3674a61fea27fa0e879239a7e662bbe31
Business-Wizard <jowi.legalia@gmail.com>
2020-10-24 23:05:39 -0500

    reorganized src .py files

--- src/EDA.py
+++ src/EDA.py
@@ -9,22 +9,17 @@ import EDA_functions as eda
 
 if __name__ == '__main__':
     linkedin_path = '../data/linkedin_leak/linkedin_hash_plain.txt'
-    sample_10m_path = '../data/10m_sample_common_passwords/10-million-combos.txt'
-    write_path = '../data/linkedin_leak/tester1.txt'
-
-    reader = pd.read_csv(linkedin_path, chunksize=1000, sep='\n')
-    # print(reader.__next__() )
-    
-
-    reader = pd.read_csv(sample_10m_path, chunksize=1000, sep='\n')
-    # print(reader.__next__() )
+    linkedin_test_file = '../data/linkedin_leak/test_write.txt'
+    test_read = '../data/10m_sample_common_passwords/test_read.txt'
+    test_write = '../data/10m_sample_common_passwords/test_write.txt'
+    sample_10m = '../data/10m_sample_common_passwords/10-million-combos.txt'
+    rockyou_path = '../data/rockyou_leak/rockyou_copy.txt'
 
 
-    # reader10m = pd.read_csv(sample_10m_path,sep='\t', header=None, chunksize=200000)
-    # * Successful
-    # ? how to detect difference in data?
-    # for chunk in reader10m:
-        # chunk.to_csv('test_write.csv', mode='a')
+    # df_linkedin = pd.read_csv(linkedin_path, header=None, sep='\n')
+    # df_rockyou = pd.read_csv(rockyou_path, sep='\n')
+    df_10msample = pd.read_csv(sample_10m, header=None, sep='\t')
+
 
 
 
--- src/EDA_functions.py
+++ src/EDA_functions.py
@@ -12,22 +12,13 @@ def stringify_row(row: str):
 def stringify_rows(data: list):
     return map(stringify_row, data)
 
-
 def chunk_to_csv(*, file_in: str, file_out: str, chunksize:int=10000):
     reader = pd.read_csv(file_in, chunksize=chunksize, sep=',')
     for chunk in reader:
         result = chunk
         result.to_csv(file_out, mode='a', index=False, header=False)
 
-if __name__ == '__main__':
-    linkedin_path = '../data/linkedin_leak/linkedin_hash_plain.txt'
-    linkedin_test_file = '../data/linkedin_leak/test_write.txt'
-    test_read = '../data/10m_sample_common_passwords/test_read.txt'
-    test_write = '../data/10m_sample_common_passwords/test_write.txt'
-    sample_10m = '../data/10m_sample_common_passwords/10-million-combos.txt'
-    rockyou_path = '../data/rockyou_leak/rockyou_copy.txt'
-
-    '''
+def text_to_csv():
     with open(test_write, 'w') as writer:
         with open(test_read, 'r') as data:
             # csvwriter = csv.writer(writer)
@@ -43,7 +34,10 @@ if __name__ == '__main__':
                 if iter > 20:
                     break
                 iter += 1
-    '''
-    # df_linkedin = pd.read_csv(linkedin_path, header=None, sep='\n')
-    # df_10msample = pd.read_csv(sample_10m, header=None, sep='\t')
-    # df_rockyou = pd.read_csv(rockyou_path, sep='\n')
+
+
+if __name__ == '__main__':
+    pass
+
+
+
